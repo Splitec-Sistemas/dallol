41b1a6e47cba071a5bd88a106d46e78d554493d6
AlessandroToledo <alematheustoledo@gmail.com>
2024-03-21 14:39:16 -0300

    fixing problem when try to run the app

--- app/src/main/java/com/example/smartwatchapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/smartwatchapp/presentation/MainActivity.kt
@@ -15,14 +15,9 @@ import com.example.smartwatchapp.PermissionManager
 import com.example.smartwatchapp.R
 
 class MainActivity : ComponentActivity() {
-    private lateinit var sunHealth: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //falta o código para sincronizar os dados com o celular e ai atualizar o sunHealth
-        sunHealth = findViewById(R.id.healNumber)
-
         permissionVerify()
         startLightSensor()
     }
@@ -30,7 +25,7 @@ class MainActivity : ComponentActivity() {
     //Inicia o serviço em segundo plano do sensor de luz
     private fun startLightSensor() {
         val serviceIntent = Intent(this, LightSensorService::class.java)
-        this.startService(serviceIntent)
+        startService(serviceIntent)
     }
 
     // Realiaza a verificação das permissões para o uso de notificações e do gps
